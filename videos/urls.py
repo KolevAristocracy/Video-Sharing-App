dd6041ddad9107ab633f49dfb5e7a21d72aeaa7b
Kalin Kolev <kalink300@gmail.com>
2025-09-01 16:14:09 +0300

    feat: Added Video model, urls, admin panel

---
+++ videos/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from videos import views as video_views
+
+urlpatterns = [
+    path('', video_views.index, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
